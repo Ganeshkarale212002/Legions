cd3f6d73f96a536f50061e1c46cccedd410ea8da
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2023-03-23 17:37:09 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -21,12 +21,12 @@ disabled_group=filters.create(disabled_chat)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'🚫<b> Honey, You Are Banned From Using Me </b> \n❗<b>Why ?</b> {ban["ban_reason"]} 🙁 \n\n<b>📲 Contact @HeroFeedbot if You Want To Get Unbanned</b>')
+    await message.reply(f'🚫<b> You Are Banned From Using Me </b> \n❗<b>Why ?</b> {ban["ban_reason"]} 🙁 \n\n<b>📲 Contact @Butcher_v1_bot if You Want To Get Unbanned</b>')
 
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('📱Support📱', url=f'https://telegram.me/HeroFeedbot')
+        InlineKeyboardButton('📱Support📱', url=f'https://telegram.me/Butcher_V1_bot')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
