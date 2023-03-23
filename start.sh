d06b9f19d0e55fbf34c5125168f786f44e492b4f
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2023-03-23 18:59:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeenOfFire/Sydney /Sydney
+  git clone https://github.com/Ganeshkarale212002/Private /Private
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Sydney
+  git clone $UPSTREAM_REPO /Private
 fi
 cd /Sydney
 pip3 install -U -r requirements.txt
