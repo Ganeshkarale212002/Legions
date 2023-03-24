fa8c7ee5e6a51f9a496e7c619ec48edfbb16bb2b
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2023-03-24 14:51:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Private
 fi
-cd /Sydney
+cd /Private
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
